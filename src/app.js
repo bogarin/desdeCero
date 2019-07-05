50d057a8132a95c276aecc88c9eff84aa1a1c159
Jose Ramón Bogarin <bogarin617@gmail.com>
2019-07-04 18:12:01 -0700

    creación de docker-compose para mongodb y configuración con nodejs

--- src/app.js
+++ src/app.js
@@ -1,22 +1,65 @@
 'use estrict'
 const express = require('express')
-const bodyParse = require('body-parser')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const Products = require('../models/product.js')
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+
 const port = process.env.PORT || 3001
+const dbname = process.env.NAME_DATABASE || 'shop'
 
-app.get('/hola',(req, res)=> {
-    res.send({message:'Hola Mundo'})
+mongoose.connect(`mongodb://localhost:1717/${dbname}`, {useNewUrlParser: true})
+//product
+app.get('/api/product',(req, res)=>{
+    Products.find({},(err,products)=>{
+        if(err) res.status(404).send({message:`Problemas: ${err}`})
+  
+        res.status(200).send(products)
+    })
 })
 
-app.get('/hola/:name',(req, res)=> {
-    res.send({message:`Hola ${req.params.name}`})
+app.get('/api/product/:productId',(req, res)=>{
+    let productId=req.params.productId
+    Products.findById(productId, (err,product)=>{
+        if(err) res.status(404).send({message:`Producto no existe`})
+
+        res.status(200).send(product)
+    })
 })
 
-app.use(bodyParse.urlencoded({extended: false}))
-app.use(bodyParse.json())
+app.post('/api/product',(req, res) => {
+    console.log(req.body)
+    let product = new Products()
+    product.name = req.body.name
+    product.picture = req.body.picture
+    product.price = req.body.price
+    product.category = req.body.category
+    product.description = req.body.description
+
+    product.save((err,productStored)=>{
+        if(err) res.status(500).send({message:`Error al almacenar los datos${err}`})
+
+        res.status(200).send(productStored)
+    })
+})
+
+app.put('/api/product/:productId',(req, res)=>{
+    
+})
+
+
+app.delete('/api/product/:productId',(req, res)=>{
+    
+})
+
 
 app.listen(port,()=>{
     console.log(`Trabajando en el purto:${port}`)
     console.log(`http://localhost:${port}`)
+    console.log(`trabajando enla base de datos: ${dbname}`)
 })
