fda2d17638dbf4bb1be3aac9f0986fcf0cccf68c
Jose Ramón Bogarin <bogarin617@gmail.com>
2019-07-14 00:31:28 -0700

    cambios para heroku

--- config/configENV.js
+++ config/configENV.js
@@ -3,8 +3,8 @@ require('dotenv/config')
 module.exports={
     port: process.env.PORT_NODE || 3001,
     dbname: process.env.NAME_DATABASE_NODE || 'shop',
-    portdb: process.env.PORT_DOCKER_MONGODB || 1717,
-    urlmongodb: process.env.URL_MONGODB || '//localhost:',
+    portdb: process.env.PORT_DOCKER_MONGODB ,
+    urlmongodb: process.env.MONGODB_URI || '//localhost:',
     urlapi: process.env.URL_API || '//localhost:',
     secret: process.env.SECRET_TOKEN
 }
