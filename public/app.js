f4894ede9a8c9c0a04b84cad307e59b0fb42e76d
Jose Ramón Bogarin <bogarin617@gmail.com>
2019-07-14 21:28:05 -0700

    cambios para heroku

--- public/app.js
+++ public/app.js
@@ -4,22 +4,21 @@ const mongoose = require('mongoose')
 const app = require('../routes/routes')
 
 if(config.mode != 'develop'){
-    mongoose.connect(config.urlmongodb)
+    mongoose.connect(config.urlmongodb,{ useNewUrlParser: true })
     .then(db => console.log('connect'))
     .catch(err=> console.log(`No conecto ${err}`))
     app.listen(config.port)
-}
-
-mongoose.connect(`mongodb:${config.urlmongodb}${config.portdb}/${config.dbname}`,{useNewUrlParser: true},(err,res)=>{
-    if(err) {
-        return console.log(`Error al conectarse ${err}`)
-    }
-    console.log('Conexion exitosa....')
-    app.listen(config.port,()=>{
-        console.log(`Trabajando en el purto:${config.port}`)
-        console.log(`http:${config.urlapi}${config.port}`)
-        console.log(`trabajando enla base de datos: ${config.dbname}`)
+}else{
+    mongoose.connect(`mongodb:${config.urlmongodb}${config.portdb}/${config.dbname}`,{useNewUrlParser: true},(err,res)=>{
+        if(err) {
+            return console.log(`Error al conectarse ${err}`)
+        }
+        console.log('Conexion exitosa....')
+        app.listen(config.port,()=>{
+            console.log(`Trabajando en el purto:${config.port}`)
+            console.log(`http:${config.urlapi}${config.port}`)
+            console.log(`trabajando enla base de datos: ${config.dbname}`)
+        })
     })
-})
-
+}
 
